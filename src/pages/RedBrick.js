925bf913f8b20e49db1dc0799297da8c4106fd38
WongueShin <won7963kr@gmail.com>
2022-01-25 16:20:13 +0900

    Modify RedBrick.js -드롭다운 메뉴를 컴포넌트 파일 단위로 분리하고, 환율을 표시해줄 컴포넌트를 임포트 시켰습니다.

--- src/pages/RedBrick.js
+++ src/pages/RedBrick.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import CurrencyDropDown from './CurrencyDropDown';
+import ExChangeRateDisPlay from './ExcahngeRateDisplay';
 
 const RedBrick = () => {
+  const [baseCurrency, setBaseCurrency] = useState(0)
   return (
     <>
-      <CurrencyDropDown/>
+      <CurrencyDropDown base = {baseCurrency} baseSetter = {setBaseCurrency}/>
+      <ExChangeRateDisPlay base = {baseCurrency} baseSetter = {setBaseCurrency}/>
     </>
   );
 };
