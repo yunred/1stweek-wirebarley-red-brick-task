1291fe4f381bb07d36753f7f8b19f563e08bdb1f
WongueShin <won7963kr@gmail.com>
2022-01-25 21:40:41 +0900

    Style: ExchangeRateDisplay.js CSS 작업을 완료했습니다!

--- src/pages/ExchangeRateDisplay.js
+++ src/pages/ExchangeRateDisplay.js
@@ -5,11 +5,13 @@ const ExChangeRateDisPlay = (props) => {
     const baseCurrency = props.base;
     const amount = props.amount;
     const time = props.time;
+    console.log(time);
     const quotesdata = props.quotesdata;
     const currencyList = ['USD', 'CAD', 'KRW', 'HKD', 'JPY', 'CNY'];
-    const [selectedCurrency , setSelectedCurrency] = useState(baseCurrency == currencyList.length? 0: baseCurrency + 1);
+    const monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    const [selectedCurrency , setSelectedCurrency] = useState(baseCurrency === currencyList.length? 0: baseCurrency + 1);
     useEffect(()=>{
-      setSelectedCurrency(baseCurrency == 0? 1: 0)
+      setSelectedCurrency(baseCurrency === 0? 1: 0)
     }, [baseCurrency]);
 
     const selectList = []
@@ -34,7 +36,9 @@ const ExChangeRateDisPlay = (props) => {
               {selectList}
           </DisplaySelector>
           <InfoDiv>
-            <span>{`${currencyList[selectedCurrency]} ${amount? baseCurrency === 0? amount*quotesdata[selectedCurrency]: amount/quotesdata[baseCurrency]*quotesdata[selectedCurrency] : ''}`}</span>
+            <Exchanged><span style={{"marginRight":"5px"}}>{currencyList[selectedCurrency]}</span> <span>{amount? baseCurrency === 0? (amount*quotesdata[selectedCurrency]).toFixed(2): (amount/quotesdata[baseCurrency]*quotesdata[selectedCurrency]).toFixed(2) : ''}</span></Exchanged>
+            <Time>기준일 :<br/>
+            {`${time.getUTCFullYear()}-${monthList[time.getUTCMonth() - 1]}-${time.getDate()>10? time.getDate(): `0${time.getDate()}`}`}</Time>
           </InfoDiv>
         </Display>
       </>
@@ -57,15 +61,43 @@ const DisplaySelector = styled(UL)`
     border-right: none;
     border-bottom: none;
     display: flex;
+    
     width: fit-content;
     `
   const Display = styled.div`
   width: fit-content;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
   `
+   const Exchanged = styled.span`
+   margin-top: 1rem;
+   padding-left: 1rem;
+   font-weight: 700;
+   font-size: 1.2em;
+   box-sizing: border-box;
+   display: flex;
+   flex-wrap: wrap;
+   `
   const InfoDiv = styled.div`
   border: solid 3px rgba(1,1,1,1);
   border-top: none;
   height: 15em;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  ${Exchanged}{
+    width:calc(12em);
+  }
   `
   
+ 
+  
+  const Time = styled.span`
+  font-size: 0.9rem;
+  font-weight: 700;
+  padding-left: 1rem;
+  margin-top: 1rem;
+  `
+
   export default ExChangeRateDisPlay;
