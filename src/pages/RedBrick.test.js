3368d83aedaf6f611ecac9ef61e53d87e6bff4a7
WongueShin <won7963kr@gmail.com>
2022-01-25 15:23:19 +0900

    Add: CurrenyDropDown.js 기준통화를 설정하는 드롭다운 매뉴 컴포넌트를 추가했습니다!

---
+++ src/pages/RedBrick.test.js
@@ -0,0 +1,7 @@
+import { render, screen} from "@testing-library/react";
+import RedBrick from "./RedBrick";
+
+test('RedBrick Render', ()=> {
+    render(<RedBrick />);
+    const testText = screen.getByLabelText("와이어바일리입니다");
+})
