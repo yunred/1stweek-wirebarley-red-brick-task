ac986d0795f08e53aeb27cb1c47a25b82ac63643
SEOYUN KIM <nado0903@gmail.com>
2022-01-25 11:20:39 +0900

    Add: utils/hooks 폴더 useInterval.js 파일 생성

--- src/pages/WireBarley.js
+++ src/pages/WireBarley.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import UseFetchExchangeRate from '../utils/axios/useFetchExchangeRate';
+import useInterval from '../utils/hooks/useInterval';
 
 const WireBarley = () => {
-  return <div>와이어바일리입니다</div>;
+  useInterval(() => {
+    UseFetchExchangeRate().then(data => console.log(data.quotes));
+  }, 3000);
+  return (
+    <>
+      <div>와이어바일리입니다</div>
+    </>
+  );
 };
 
 export default WireBarley;
