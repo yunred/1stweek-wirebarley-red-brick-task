03f57f8d3eb29d1a91f6b4535825533543f98bdd
SEOYUN KIM <nado0903@gmail.com>
2022-01-25 13:04:51 +0900

    Fix: 서버 요청 수정

--- src/pages/WireBarley.js
+++ src/pages/WireBarley.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import UseFetchExchangeRate from '../utils/axios/useFetchExchangeRate';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import UseFetchExchangeRate from '../utils/fetch/useFetchExchangeRate';
 import useInterval from '../utils/hooks/useInterval';
 
 const WireBarley = () => {
-  useInterval(() => {
+  const [intervalFlag, setIntervalFlag] = useState(true);
+  useEffect(() => {
     UseFetchExchangeRate().then(data => console.log(data.quotes));
+  }, [intervalFlag]);
+  useInterval(() => {
+    setIntervalFlag(!intervalFlag);
   }, 3000);
   return (
     <>
+      <Link to="/RedBrick">두 번째 계산기</Link>
       <div>와이어바일리입니다</div>
     </>
   );
