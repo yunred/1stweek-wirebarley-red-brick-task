442253837b6a715bc2368c944d608d2f7e455bb3
SEOYUN KIM <nado0903@gmail.com>
2022-02-01 00:28:35 +0900

    fix: 소수점 입력 기능 추가

--- src/pages/RedBrick.js
+++ src/pages/RedBrick.js
@@ -3,7 +3,6 @@ import ExChangeRateDisPlay from './ExchangeRateDisplay';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-
 const Inputbox = styled.input`
   border: solid 2px black;
   text-align: center;
@@ -13,50 +12,76 @@ const Inputbox = styled.input`
   width: 7rem;
 `;
 
-const RedBrick = (props) => {
-  const [baseCurrency, setBaseCurrency] = useState(0)    
+const RedBrick = props => {
+  const [baseCurrency, setBaseCurrency] = useState(0);
   const [amount, setAmount] = useState();
-  const onInputAmount = (e) => {
+  const [pointFlag, setPointFlag] = useState(0);
+
+  function toNumberFormat(num) {
+    if (pointFlag === 1) {
+      String(num.toLocaleString('ko-KR') + '.');
+    } else {
+      return num.toLocaleString('ko-KR');
+    }
+  }
+
+  const onInputAmount = e => {
     e.preventDefault();
-    setAmount(e.target.value.replace(/[^0-9]/g, ""));
-    console.log(amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    let num = String(e.target.value.replace(/[^0-9.]/g, ''));
+    if (num[num.length - 1] === '.') {
+      setPointFlag(1);
+      setAmount(num.substring(0, num.length - 1));
+    } else {
+      setPointFlag(0);
+      setAmount(num);
+    }
   };
 
   const jsonData = props.data;
-  const quotesdata =  [1, jsonData.quotes["USDCAD"],jsonData.quotes["USDKRW"], jsonData.quotes["USDHKD"], jsonData.quotes["USDJPY"], jsonData.quotes["USDCNY"]];
+  const quotesdata = [
+    1,
+    jsonData.quotes['USDCAD'],
+    jsonData.quotes['USDKRW'],
+    jsonData.quotes['USDHKD'],
+    jsonData.quotes['USDJPY'],
+    jsonData.quotes['USDCNY'],
+  ];
   const time = new Date(parseInt(jsonData.timestamp) * 1000);
   return (
     <Container>
       <Inputbox
         type="text"
         onChange={onInputAmount}
-        value={
-          amount && amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-        }
+        value={amount && toNumberFormat(Number(amount))}
       />
       <DropDown>
-        <CurrencyDropDown base = {baseCurrency} baseSetter = {setBaseCurrency}/>
+        <CurrencyDropDown base={baseCurrency} baseSetter={setBaseCurrency} />
       </DropDown>
-      <ExChangeRateDisPlay base = {baseCurrency} amount={amount} time = {time} quotesdata = {quotesdata}/>
+      <ExChangeRateDisPlay
+        base={baseCurrency}
+        amount={amount}
+        time={time}
+        quotesdata={quotesdata}
+      />
     </Container>
   );
 };
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-width: fit-content;
-height: fit-content;
-margin-top: 5rem;
-margin-left: 2rem;
-padding: 1rem;
-border: solid 3px rgb(1,1,1);
-`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  height: fit-content;
+  margin-top: 5rem;
+  margin-left: 2rem;
+  padding: 1rem;
+  border: solid 3px rgb(1, 1, 1);
+`;
 
 const DropDown = styled.div`
-position: absolute;
-left: 14rem;
-top: 5.2rem
-`
+  position: absolute;
+  left: 14rem;
+  top: 5.2rem;
+`;
 
 export default RedBrick;
